d878f20102305272bfc59478f387d07407d267c6
hanhdungdao_ouhsc <hanhdung.dao@gmail.com>
2021-09-27 13:36:34 -0500

    create 01_clean_data.r

---
+++ scripts/01_clean_data.R
@@ -0,0 +1,6 @@
+# ------------------------------------------------------------------------------
+# Title: Script to clean data necessary for Aim 1
+# Author: Hanh Dung Dao
+# Purpose: To import and clean variables for exposure (measured indoor environmental quality, IEQ), outcome (student standardized test scores) and covariate (TBA, tk)
+# ------------------------------------------------------------------------------
+
