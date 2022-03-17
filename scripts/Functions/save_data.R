aecd452bcb070c1b9a1a31b6a75f3a7a62ea6f4d
Dao <HanhDung-Dao@ouhsc.edu>
2022-03-17 13:22:29 -0500

    create function save_data()

---
+++ scripts/Functions/save_data.R
@@ -0,0 +1,30 @@
+save_data <- function(dataset.name, file.location, file.location.arc,
+                      csv = TRUE, sas = FALSE, xlsx = FALSE){
+  saveRDS(dataset.name, file = paste0(file.location, ".rds"))
+  saveRDS(dataset.name, file = paste0(file.location.arc, format(Sys.Date(), "_%Y%m%d"), ".rds"))
+
+  # CSV
+  if(csv) {
+    readr::write_csv(dataset.name, paste0(file.location, ".csv"))
+    readr::write_csv(dataset.name, paste0(file.location.arc, format(Sys.Date(), "_%Y%m%d"), ".csv"))
+  }
+
+  # SAS
+  if(sas) {
+    dataset.name %<>% dplyr::mutate(dplyr::across(where(is.factor), as.character))
+    foreign::write.foreign(dataset.name,
+                           datafile = paste0(file.location, ".txt"),
+                           codefile = paste0(file.location, ".sas"),
+                           package = "SAS")
+    foreign::write.foreign(dataset.name,
+                           datafile = paste0(file.location.arc, format(Sys.Date(), "_%Y%m%d"), ".txt"),
+                           codefile = paste0(file.location.arc, format(Sys.Date(), "_%Y%m%d"), ".sas"),
+                           package = "SAS")
+  }
+
+  # XLSX
+  if(xlsx) {
+    openxlsx::write.xlsx(dataset.name,paste0(file.location, ".xlsx"))
+    openxlsx::write.xlsx(dataset.name,paste0(file.location.arc, format(Sys.Date(), "_%Y%m%d"), ".xlsx"))
+  }
+}
