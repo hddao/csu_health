ebbd885ffa98939fe60a64c2594b7175b14755b6
HDD_MikeNUC <hanhdung.dao@gmail.com>
2022-03-26 15:58:46 -0500

    update scatterplots

--- scripts/204_tables_and_figures.R
+++ scripts/204_tables_and_figures.R
@@ -45,37 +45,33 @@ scatterplot <- gs_desc_df %>%
     plot_list <- list %>%
       purrr::map(.f = function(df){
         ggplot2::ggplot(df) +
+          ggplot2::geom_abline(slope = 1, intercept = 0, colour = "grey50", size = 1.0) +
           ggplot2::geom_point(ggplot2::aes(x = get(names(df)[4]),
                                            y = get(names(df)[5]),
                                            colour = `Radius (m)`),
-                              shape = 4) +
+                              shape = 4, size = 0.8) +
           ggplot2::xlim(0, 0.6) +
           ggplot2::ylim(0, 0.6) +
-          ggplot2::geom_abline(slope = 1, intercept = 0) +
           ggplot2::xlab(names(df)[4]) +
           ggplot2::ylab(names(df)[5]) +
           ggplot2::theme_bw() +
-          ggplot2::theme(legend.position = c(.95, .95),
-                         legend.justification = c("right", "top"),
+          ggplot2::theme(legend.position = c(0.95, 0.05),
+                         legend.justification = c("right", "bottom"),
                          legend.box.just = "left") +
           ggplot2::theme(legend.background = ggplot2::element_rect(fill="gray90")) +
           ggsci::scale_color_nejm()})
     plot_list
   })
 
-p1 <- scatterplot[[1]][[1]] +
-  ggplot2::xlab("MODIS") +
-  ggplot2::ylab("NLCD")
-p2 <- scatterplot[[1]][[2]] +
-  ggplot2::xlab("Landsat 8") +
-  ggplot2::ylab("MODIS")
-p3 <- scatterplot[[1]][[3]] +
-  ggplot2::xlab("Landsat 8") +
-  ggplot2::ylab("NLCD")
+scatterplot[[1]][[1]] <- scatterplot[[1]][[1]] + ggplot2::xlab("MODIS") + ggplot2::ylab("NLCD") +
+  ggplot2::theme(legend.position = c(0.05, 0.95),
+                 legend.justification = c("left", "top"),
+                 legend.box.just = "left")
+scatterplot[[1]][[2]] <- scatterplot[[1]][[2]] + ggplot2::xlab("Landsat 8") + ggplot2::ylab("MODIS")
+scatterplot[[1]][[3]] <- scatterplot[[1]][[3]] + ggplot2::xlab("Landsat 8") + ggplot2::ylab("NLCD")
 
-
-
-list(p1, p2, p3) %>%
+tictoc::tic("ggsave")
+scatterplot[[1]] %>%
   purrr::map2(c(1:3),
               ~ggplot2::ggsave(plot = .x,
                               paste0("outputs/figures/Aim2/scatterplot_nejm_",
@@ -85,10 +81,10 @@ list(p1, p2, p3) %>%
                               width = 7,
                               height = 7,
                               units = "in"))
-
+tictoc::toc()
 
 # Agreement table ---------------------------------------------------------
-agreement_stat_df <- "DATA/Processed/Aim2/Agreement/agreement_stat_df_pairwise_allmonths.rds"
+agreement_stat_df <- readr::read_rds("DATA/Processed/Aim2/Agreement/agreement_stat_df_pairwise_allmonths.rds")
 quantile_list <- readr::read_rds("DATA/Processed/Aim2/Agreement/Bootstrap/quantile_list.rds")
 
 
@@ -98,9 +94,14 @@ pair_df <- tibble::tibble(landsat_26953 = c(0,1,1),
                           modis_26953 = c(1,1,0),
                           pair = c("MODIS & NLCD", "Landsat 8 & MODIS", "Landsat 8 & NLCD"))
 
+# Create a reference table for agreement stats name
+agreement_desc_df <- tibble::tibble(
+  agreement_stats = c("meanb", "msd", "cp_05", "cp_10", "tdi_05", "tdi_10", "ccc"),
+  `Agreement Statistics` = c("LOA", "MSD", "CP 0.05", "CP 0.10", "TDI 0.05", "TDI 0.10", "CCC")
+)
+
 
 agreement_stat_df <- agreement_stat_df %>%
-  readr::read_rds() %>%
   dplyr::rename_all(tolower) %>%
   # change cp and tdi from list to numeric
   dplyr::mutate(cp_05 = cp %>% purrr::map(dplyr::first) %>% as.numeric(),
@@ -185,7 +186,7 @@ agreement_by_month <- agreement_stat_df %>%
 
 # Plot agreementbymonth and export
 plot <- agreement_by_month %>%
-  purrr::walk(function(df) {
+  purrr::map(function(df) {
     # split data to scale and unscale stats
     df_scale <- df %>%
       dplyr::filter(`Agreement Statistics` %in% c("CCC", "CP 0.05", "CP 0.10"))
@@ -213,7 +214,7 @@ plot <- agreement_by_month %>%
       rep(2) %>%
       stringr::str_c(c("_scale", "_unscale"))
     plot_list %>%
-      purrr::walk2(suffix,
+      purrr::map2(suffix,
                    ~ggplot2::ggsave(plot = .x,
                                     paste0("outputs/figures/Aim2/agreementbymonth_",
                                            .y,
@@ -222,12 +223,12 @@ plot <- agreement_by_month %>%
                                     width = 5,
                                     height = 5,
                                     units = "in"))
+    plot_list
     })
 
 
 
 # Bland-Altman plots ------------------------------------------------------
-# Bland-Altman plot -------------------------------------------------------
 
 gs_all_pair_list <- readr::read_rds("DATA/Processed/Aim2/Agreement/gs_all_pair_list.rds")
 quantile_list <- readr::read_rds("DATA/Processed/Aim2/Agreement/Bootstrap/quantile_list.rds")
