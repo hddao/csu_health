293fc3026def2ef122e4b7886aa7afdb970451d2
Dao <HanhDung-Dao@ouhsc.edu>
2022-04-08 17:49:32 -0500

    update save_data()

--- scripts/202a_prepare_data_for_NDVI_avg.R
+++ scripts/202a_prepare_data_for_NDVI_avg.R
@@ -1,37 +1,8 @@
 rm(list = ls())
 
 # Functions ---------------------------------------------------------------
+source("scripts/Functions/save_data.R")
 
-save_data <- function(dataset.name, file.location, file.location.arc,
-                      csv = TRUE, sas = FALSE, xlsx = FALSE){
-  saveRDS(dataset.name, file = paste0(file.location, ".rds"))
-  saveRDS(dataset.name, file = paste0(file.location.arc, format(Sys.Date(), "_%Y%m%d"), ".rds"))
-
-  # CSV
-  if(csv) {
-    readr::write_csv(dataset.name, paste0(file.location, ".csv"))
-    readr::write_csv(dataset.name, paste0(file.location.arc, format(Sys.Date(), "_%Y%m%d"), ".csv"))
-  }
-
-  # SAS
-  if(sas) {
-    dataset.name %<>% dplyr::mutate(dplyr::across(where(is.factor), as.character))
-    foreign::write.foreign(dataset.name,
-                           datafile = paste0(file.location, ".txt"),
-                           codefile = paste0(file.location, ".sas"),
-                           package = "SAS")
-    foreign::write.foreign(dataset.name,
-                           datafile = paste0(file.location.arc, format(Sys.Date(), "_%Y%m%d"), ".txt"),
-                           codefile = paste0(file.location.arc, format(Sys.Date(), "_%Y%m%d"), ".sas"),
-                           package = "SAS")
-  }
-
-  # XLSX
-  if(xlsx) {
-    openxlsx::write.xlsx(dataset.name,paste0(file.location, ".xlsx"))
-    openxlsx::write.xlsx(dataset.name,paste0(file.location.arc, format(Sys.Date(), "_%Y%m%d"), ".xlsx"))
-  }
-}
 
 # Load data ---------------------------------------------------------------
 load_nlcd <- terra::rast("DATA/Processed/Aim2/NLCD/aim2_nlcd16_e4000_raster.tif")
