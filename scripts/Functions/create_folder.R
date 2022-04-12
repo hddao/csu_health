dcb44b4793f4040791311609d3faff75187407d3
HDD_MikeNUC <hanhdung.dao@gmail.com>
2022-04-11 22:55:20 -0500

    edit create_folder.R

--- scripts/Functions/create_folder.R
+++ scripts/Functions/create_folder.R
@@ -1,4 +1,8 @@
+# create_folder <- function(mainDir, subDir) {
+#   if(!(dir.exists(file.path(subDir)))) {dir.create(file.path(mainDir, subDir))}
+#   else {print("directory already exists")}
+# }
+
 create_folder <- function(mainDir, subDir) {
-  if(!(dir.exists(file.path(subDir)))) {dir.create(file.path(mainDir, subDir))}
-  else {print("directory already exists")}
+  dir.create(file.path(mainDir, subDir))
 }
