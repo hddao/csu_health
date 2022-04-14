252553ce2f733dfefab0e4d88e6fa5fb7040e6f6
HDD_MikeNUC <hanhdung.dao@gmail.com>
2022-04-14 13:51:53 -0500

    clean codes

--- scripts/202g_calculate_NDVI_avg_summerclear.R
+++ scripts/202g_calculate_NDVI_avg_summerclear.R
@@ -151,7 +151,6 @@ list_df <- list_df %>%
 # only run buffer from 25m to 1000m
 list_df[1:8, ] %>%
   dplyr::filter(!(distance_chr %in% c("2000", "4000"))) %>%
-  dplyr::filter((distance_chr %in% c("500", "1000"))) %>%
   purrr::pwalk(.f = export_greenspace_df_long)
 # RANGE export greenspace: 0.2 - 5.0 sec elapsed
 
